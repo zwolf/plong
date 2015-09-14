b218030f2819249ce93e824ddad14a568a323f4c
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2015-09-14 17:52:03 -0500

    Initial commit!

---
+++ log_faker.rb
@@ -0,0 +1,93 @@
+require 'date'
+
+def run
+  loop do
+    # Changes to a file wont appear until the file is closed, which is why we have to open/close it each time.
+    ARGV.each do |f|
+      write_log(f)
+    end
+    sleep (1..4).to_a.sample
+  end
+end
+
+def write_log(filename)
+  File.open("./logs/#{filename}", "a") do |file|
+    ((1..4).to_a.sample).times do
+      file.write("#{request}\n")
+    end
+  end
+end
+
+def request
+  %Q(127.0.0.1 - - [#{DateTime.now.strftime("%d\/%b\/%Y:%H:%M:%S\s-0700")}] "GET #{path.sample} HTTP/1.1" #{status_codes.sample} #{Random.new.rand(1000..50000)} "#{domains.sample}" "#{user_agents.sample}" "#{random_ip}")
+end
+
+def domains
+  [
+    "http://www.usw.org/",
+    "http://www.atu.org/",
+    "http://www.trilogyinteractive.com/",
+    "http://www.theunion.org/",
+    "http://www.cadem.org/",
+    "http://www.dickdurbin.com/",
+    "http://www.afscme.org/",
+  ]
+end
+
+def status_codes
+  # Common response codes appear more so #sample gives a more realistic distribution of responses.
+  [
+    "200", "200", "200", "200", "200", "200", "200", "200", "200", "200",
+    "404", "404", "404", "404",
+    "206", "206", 
+    "410", "502", "500", "410",
+  ]
+end
+
+def random_ip
+  random = Random.new
+  ip = String.new
+
+  4.times do |i|
+    [1,2,3].sample.times do
+      ip << random.rand(0..9).to_s
+    end
+
+    ip << "." unless i == 3
+  end
+
+  ip
+end
+
+def path
+  [
+    "/theme/css/nav.css",
+    "/theme/css/style.css",
+    "/theme/scripts/site.js",
+    "/theme/scripts/jquery.js",
+    "/theme/img/logo.png",
+    "/theme/img/home.png",
+    "/theme/img/blank.gif",
+    "/",
+    "/about",
+    "/about/staff",
+    "/about/contact",
+    "/blog",
+    "/blog/our-first-post",
+    "/blog/our-last-post",
+    "/issues/some/deeply/nested/issue"
+  ]
+end
+
+def user_agents
+  [
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/7.1.7 Safari/537.85.16",
+    "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Android; Tablet; rv:38.0) Gecko/38.0 Firefox/38.0",
+    "Mozilla/5.0 (iPad; CPU OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12H321 Safari/600.1.4",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+  ]
+end
+
+### Start running
+run
---
+++ parser.pl
@@ -0,0 +1,105 @@
+#!/usr/bin/perl -w
+
+# USAGE: ./parser.pl [log1] [log2] [...]
+# It'll use "access.log" and "error.log" by default.
+
+# Logs should be stored in ./logs and faked with the ruby script
+
+use strict;
+use warnings;
+
+use MongoDB ();
+use Parse::AccessLog;
+use Data::Dumper qw(Dumper);
+use 5.010;
+use autodie 'open';
+use POE qw(Wheel::FollowTail);
+
+# Set up shared mongodb connection
+my $client = MongoDB::MongoClient->new(host => 'localhost', port => 27017);
+my $db = $client->get_database('parsed_logs');
+
+# Iterate through arguments as filenames if they exist
+# OR, iterate through default array
+my @logs;
+if (@ARGV) {
+  @logs = @ARGV;
+} else {
+  @logs = ('site.access.log', 'site.error.log');
+}
+
+# Create a new POE session for each file
+for my $file (@logs) {
+  say "Creating new session for $file...";
+  &create_session($file);
+}
+
+# Run the POE kernel
+$poe_kernel->run();
+
+# The basic POE session creation engine
+sub create_session {
+  my $filename = $_[0];
+  POE::Session->create(
+    inline_states => {
+      _start => sub {
+        # Open the file for tailing
+        $_[HEAP]{wheel} = POE::Wheel::FollowTail->new(
+          Filename   => "./" . $filename,
+          InputEvent => 'got_line',
+          ResetEvent => "got_log_rollover",
+          SeekBack   => 8192,
+        );
+        # Open the collection the mongo collection
+        $_[HEAP]{collection} = $db->get_collection($filename);
+      },
+      got_line => sub {
+        my $parsed = parse_line($_[ARG0]);
+        insert_into_db($parsed, $_[HEAP]{collection});
+
+      },
+      got_log_rollover => sub {
+        say "Log rolled over.";
+      },
+    },
+  );
+
+}
+
+# Misc subroutines to do the work
+sub insert_into_db {
+  my ($data, $collection) = @_;
+  $collection->insert($data);
+}
+
+sub parse_line {
+  my ($user, $loggedat, $method, 
+      $path, $status, $bytessent, 
+      $referer, $useragent, $ip, 
+      $raw) = ( $_[0] =~ q(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s-\s(.+)\s\[(.+)\]\s\"(\w+)\s(.+)\sHTTP/\d.\d\"\s(\d{3})\s(\d+)\s\"(http.+)\"\s\"(Mozilla.+)\"\s\"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\") );
+  my %parsed = (
+    "user" => $user, 
+    "loggedAt" => $loggedat, 
+    "method" => $method, 
+    "path" => $path, 
+    "status" => $status,
+    "bytesSent" => $bytessent, 
+    "referer" => $referer,
+    "userAgent" => $useragent,
+    "ip" => $ip, 
+  );
+  return \%parsed
+}
+
+
+
+# Initial proof-of-concent version
+#use File::Tail;
+#my $db = $client->get_database('parsed_logs');
+#my $logcoll = $db->get_collection('loglines');
+#my $file = File::Tail->new("requests.log");
+#while (defined(my $line= $file->read)) {
+#  my $rec = Parse::AccessLog->parse($line);
+#  print Dumper $rec;
+#  $logcoll->insert($rec);
+#}#
